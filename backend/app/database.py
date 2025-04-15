01d32067b2ed41c0350d1cb2b73faeb5570fa7cd
Raphael Pappalardo <c.mrg.studios@gmail.com>
2025-04-15 22:27:50 +0900

    databse fix 2

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
@@ -45,7 +45,7 @@ else:
         
         # Test the connection
         with engine.connect() as conn:
-            conn.execute("SELECT 1")
+            conn.execute(text("SELECT 1"))
             logger.info("Successfully connected to the database using Session Pooler")
             
     except Exception as e:
