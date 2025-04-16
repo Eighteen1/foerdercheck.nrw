f8a9805f7d20d10dfdb64547bf5367bcb927297c
Raphael Pappalardo <c.mrg.studios@gmail.com>
2025-04-16 16:09:22 +0900

    account fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: ['http://localhost:3000', 'https://foerdercheck-nrw-frontend.vercel.app'], // Add your Vercel frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
